fe519deb154995aeec4b1a12cdcc97ed66d09422
Manali031999 <76432416+Manali031999@users.noreply.github.com>
2021-02-08 17:58:04 +0530

    Create main.go

---
+++ Postgres/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"net/http"
+	"gorm.io/gorm"
+	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"encoding/json"
+	"fmt"
+)
+
+var db *gorm.DB
+var err error
+//const DNS="root:@/form?parseTime=true"
+const DNS="postgres://postgres:9825@localhost/postgres?sslmode=disable"
+
+type User struct{
+	gorm.Model
+	FirstName string   `json:"firstname`
+	LastName string    `json:"lastname"`
+	Email string       `json:"email"`
+}
+
+func InitialMigration(){
+	db,err=gorm.Open(postgres.Open(DNS),&gorm.Config{})
+	if err!=nil{
+		fmt.Println(err)
+	}
+	db.AutoMigrate(&User{})
+}
+
+func GetUsers(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	var users []User
+	db.Find(&users)
+	json.NewEncoder(w).Encode(users)
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params:=mux.Vars(r)
+	var user User
+	db.First(&user,params["id"])
+	json.NewEncoder(w).Encode(user)
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	db.Create(&user)
+	json.NewEncoder(w).Encode(user)
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+	var user User
+	db.First(&user, params["id"])
+	json.NewDecoder(r.Body).Decode(&user)
+	db.Save(&user)
+	json.NewEncoder(w).Encode(user)
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var user User
+	db.Delete(&user, params["id"])
+	json.NewEncoder(w).Encode("The USer is Deleted Successfully!")
+}
+
+func initializeRouter(){
+	r:=mux.NewRouter()
+	r.HandleFunc("/users",GetUsers).Methods("GET")
+	r.HandleFunc("/users/{id}",GetUser).Methods("GET")
+	r.HandleFunc("/users",CreateUser).Methods("POST")
+	r.HandleFunc("/users/{id}",UpdateUser).Methods("PUT")
+	r.HandleFunc("/users/{id}",DeleteUser).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8906",r))
+}
+
+func main(){
+	InitialMigration()
+	initializeRouter()
+}
