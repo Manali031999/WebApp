bb560adf4784270a726ca46435891852d07d3e1b
Manali031999 <76432416+Manali031999@users.noreply.github.com>
2021-02-18 17:08:42 +0530

    Create waitGroup.go

---
+++ Concurrency/waitGroup.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+func makeRange(min, max int) []int {
+	a := make([]int, max-min+1)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
+}
+
+func Odd(i int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	log.Println("API call for", i, "started")
+}
+
+func Even(i int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	log.Println("API call for", i, "started")
+}
+
+func main() {
+	var wg sync.WaitGroup
+	numArray := makeRange(0, 100)
+	start := time.Now()
+	for i, _ := range numArray {
+		if i%2 == 0 {
+			wg.Add(1)
+			go Even(i, &wg)
+		} else {
+			wg.Add(1)
+			go Odd(i, &wg)
+		}
+	}
+	wg.Wait()
+	elapsed := time.Since(start)
+	log.Printf("Time taken %s", elapsed)
+}
