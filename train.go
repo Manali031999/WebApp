eca12ddd72dd47b68c415964223219a9317f21dd
Manali031999 <manali.shah@bacancy.com>
2021-02-25 15:03:05 +0530

    Pagination

--- train.go
+++ train.go
@@ -9,19 +9,22 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
-	cred           = "mongodb://localhost:27017"
-	dbname         = "test"
-	collectionName = "trainers"
-	limit          = 20
+	// cred           = "mongodb://localhost:27017"
+	// dbname         = "test"
+	// collectionName = "trainers"
+	limit = 1
 )
 
 var (
@@ -58,6 +61,9 @@ func ReadCsv(filename string) ([][]string, error) {
 }
 
 func dbConn() (*mongo.Collection, *mongo.Client) {
+	cred := goDotEnvVariable("CRED")
+	dbname := goDotEnvVariable("DBNAME")
+	collectionName := goDotEnvVariable("COLLECTION")
 	clientOptions := options.Client().ApplyURI(cred)
 	client, err := mongo.Connect(context.TODO(), clientOptions) // Connect to MongoDB
 	if err != nil {
@@ -94,14 +100,12 @@ func getallTrains(w http.ResponseWriter, r *http.Request) {
 func insert(read *bool) {
 	collection, client := dbConn()
 	defer client.Disconnect(context.TODO())
-	//read := flag.Bool("now", false, "a bool")
-	//flag.Parse()
 	if *read {
 		csvData, err := ReadCsv("Indian_railway1.csv")
 		if err != nil {
 			panic(err)
 		}
-		csvData = csvData[1:5001]
+		csvData = csvData[1:31]
 		for _, line := range csvData {
 			ch <- 1     //	wg.Add(1)
 			go func() { //defer wg.Done()
@@ -118,8 +122,7 @@ func insert(read *bool) {
 					SSname:    line[9],
 					Ds:        line[10],
 					DsName:    line[11],
-				}
-
+				} //time.Sleep(time.Second)
 				_, err := collection.InsertOne(context.TODO(), data)
 				if err != nil {
 					panic(err)
@@ -133,39 +136,44 @@ func insert(read *bool) {
 	} else {
 		fmt.Println("No file Read")
 	}
-	// csvData, err := ReadCsv("Indian_railway1.csv")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// csvData = csvData[1:5001]
-	// for _, line := range csvData {
-	// 	ch <- 1     //	wg.Add(1)
-	// 	go func() { //defer wg.Done()
-	// 		data := Data{
-	// 			TrainNo:   line[0],
-	// 			TrainName: line[1],
-	// 			SEQ:       line[2],
-	// 			Code:      line[3],
-	// 			StName:    line[4],
-	// 			ATime:     line[5],
-	// 			DTime:     line[6],
-	// 			Distance:  line[7],
-	// 			SS:        line[8],
-	// 			SSname:    line[9],
-	// 			Ds:        line[10],
-	// 			DsName:    line[11],
-	// 		}
-	//
-	// 		_, err := collection.InsertOne(context.TODO(), data)
-	// 		if err != nil {
-	// 			panic(err)
-	// 		}
-	// 		<-ch
-	// 	}() //	wg.Wait()
-	// }
-	// for i := 0; i < limit; i++ {
-	// 	ch <- 1
-	// }
+}
+
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load( /*"../main.env"*/ "main.env") // load .env file
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
+func getPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	start, _ := strconv.Atoi(params["id"])
+	start--
+	skip := int64(start * 10)
+	if skip <= 0 {
+		skip = 0
+	}
+	collection, client := dbConn()
+	defer client.Disconnect(context.TODO())
+	options := options.Find()
+	options.SetLimit(5)
+	options.SetSkip(skip)
+	cursor, err := collection.Find(context.TODO(), bson.D{{}}, options)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var trains []Data
+	if err = cursor.All(context.TODO(), &trains); err != nil {
+		log.Fatal(err)
+	}
+	bytedata, err := json.MarshalIndent(trains, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(bytedata)
 }
 
 func main() {
@@ -177,12 +185,15 @@ func main() {
 		insert(read)
 	} else {
 		fmt.Println("No file found")
-	}
-	//insert()
+	} //insert()
 	elapsed := time.Since(start)
 	fmt.Println(elapsed)
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/Trains", getallTrains)
-	http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/Trains/{id}", getPost).Methods(("GET"))
+	router.HandleFunc("/Trains", getallTrains).Methods(("GET"))
+	// fs := http.FileServer(http.Dir("static/"))
+	// http.Handle("/static/", http.StripPrefix("/static/", fs))
+	// http.HandleFunc("/Trains", getallTrains)
+	log.Fatal(http.ListenAndServe(":8080", router))
+	// http.ListenAndServe(":8080", nil)
 }
